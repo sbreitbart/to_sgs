eda0611a8d6600b5dba3ba0c4000ec885428fdb6
sbreitbart <sbreitbart@wesleyan.edu>
2023-04-19 11:53:26 -0400

    added .sh and slurm output files associated with step 6 / calculating fstats

---
+++ step06_callSNPs/ch3_alldata/fstats/ch3_filter_fstats_trial12.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=pops_fstats_filtering_trial12
+#SBATCH --time=5:00:00
+#SBATCH --mem-per-cpu=256G
+#SBATCH --account=def-wagnerh1
+#SBATCH --mail-user=sophie.breitbart@mail.utoronto.ca
+#SBATCH --mail-type=ALL
+
+module load stacks/2.62
+
+populations -P output_refmap_run2 -O output_filtering_fstats_trial12 --popmap pop_map2.txt -t 8 --hwe --fstats --plink --structure --vcf --verbose -R 1 -r 0.2 --min-maf 0.05
