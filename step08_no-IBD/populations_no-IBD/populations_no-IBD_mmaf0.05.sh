4662ddb0b9333d45adc61c60dde1889dc8d24daf
sbreitbart <sbreitbart@wesleyan.edu>
2023-06-11 17:58:06 -0400

    added 3 important files from each stacks:populations run: populations.snps.vcf populations.sumstats.tsv populations.sumstats_summary.tsv- for mmaf 0f 0.00762 and 0.5, and R of 0.5, 0.75, and 1

---
+++ step08_no-IBD/populations_no-IBD/populations_no-IBD_mmaf0.05.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=pops_fstats_no-IBD_mmaf0.05
+#SBATCH --time=20:00:00
+#SBATCH --mem-per-cpu=250G
+#SBATCH --account=def-wagnerh1
+#SBATCH --mail-user=sophie.breitbart@mail.utoronto.ca
+#SBATCH --mail-type=ALL
+
+module load stacks/2.62
+
+for min_maf in 0.05
+do
+	for R in 0.5 0.75 1
+	do
+		mkdir output_populations_no-IBD/mmaf"$min_maf"_R"$R"
+		populations -P output_refmap_no-IBD -O output_populations_no-IBD/mmaf"$min_maf"_R"$R"/ --popmap pop_map3.txt -t 8 --hwe --fstats --plink --structure --vcf --verbose -R "$R" -r 0.2 --min-maf "$min_maf" --write-single-snp
+	done
+done
