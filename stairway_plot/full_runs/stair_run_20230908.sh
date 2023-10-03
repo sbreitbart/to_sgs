9a67860c5dc9420cdb1feef36855b2d1264f286f
sbreitbart <sbreitbart@wesleyan.edu>
2023-10-03 10:08:56 -0400

    added new stairway plot files created with mmaf = 2/256 dataset

--- stairway_plot/full_runs/stair_run_20230908.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=stairwayplot
-#SBATCH --time=10:00:00
-#SBATCH --mem-per-cpu=25G
-#SBATCH --account=def-wagnerh1
-#SBATCH --mail-user=sophie.breitbart@mail.utoronto.ca
-#SBATCH --mail-type=ALL
-
-bash run_20230908.blueprint.sh
--- stairway_plot/urban_runs/urb_score/stair_urban_usc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=stairwayplot_urban
-#SBATCH --time=5:00:00
-#SBATCH --mem-per-cpu=25G
-#SBATCH --account=def-wagnerh1
-#SBATCH --mail-user=sophie.breitbart@mail.utoronto.ca
-#SBATCH --mail-type=ALL
-
-bash run_urban_usc.blueprint.sh
